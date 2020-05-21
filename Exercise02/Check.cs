0555e9f19cfbfcef93ab24f639a3852a5fb979c7
kefasogabi <kefasogabi@gmail.com>
2020-05-21 17:15:52 +0100

    Initial

---
+++ Exercise02/Check.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Check
+    {
+       
+
+        public void Compare(string input1, string input2)
+        {
+
+            if (string.IsNullOrWhiteSpace(input1))
+                input1 = @"^[a-z]+$?";
+           
+            bool containsInt = input2.Any(char.IsDigit);
+
+            if (containsInt)
+            {
+               Console.WriteLine($"{input2} matches {input1} {containsInt = false}");
+            }
+            else
+            {
+                Console.WriteLine($"{input2} matches {input1} {containsInt = true}");
+            }
+            
+        }
+    }
+}
---
+++ Exercise02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1, input2;
+            string more = "yes";
+
+            
+
+           
+            try
+            {
+
+                var check = new Check();
+                while (more == "yes")
+                {
+                    Console.Write("Enter a Regular expression ( or press Enter to  the default): ");
+                    input1 = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter some input: ");
+                    input2 = Convert.ToString(Console.ReadLine());
+                    check.Compare(input1, input2);
+
+                    Console.Write("do you want to check again (Yes/No): ");
+                    more = Convert.ToString(Console.ReadLine()).ToLower();
+                }
+                
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
